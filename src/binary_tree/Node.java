151b52c05549e98d14aa4f8248e722516206d93c
caioo <caioo@192.168.0.56>
2020-09-19 23:10:33 -0300

    Classe node Finalizada.

--- src/binary_tree/Node.java
+++ src/binary_tree/Node.java
@@ -9,16 +9,27 @@ public class Node {
     public Node() {
         //construtor para um nó pai
     }
-    
-    
-
+     
     public Node(String name, Node fatherNode) {
         //construtor para um nó filho
         this.name = name;
         this.fatherNode = fatherNode;
     }
 
-    
+    public boolean hasLeftChld(){
+        if (leftnode != null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean hasRightChld(){
+        if (rightnode != null){
+            return true;
+        }else{
+            return false;
+        }
+    }
     
     
 }
