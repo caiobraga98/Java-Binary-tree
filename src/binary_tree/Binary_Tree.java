0826118a4c25ae825d265c556378fb33a19db2ae
caioo <caioo@192.168.0.56>
2020-09-22 23:42:14 -0300

    arefatoração do código e modificação de métodos da arvore binaria.

--- src/binary_tree/Binary_Tree.java
+++ src/binary_tree/Binary_Tree.java
@@ -2,4 +2,32 @@ package binary_tree;
 
 public class Binary_Tree {
 
+    private Node upfather;
+
+    public void addNodeLeft(int value,Node pai){
+        Node aux;
+        if(pai.hasLeftChld() == true){
+            System.out.println("impossivel remover pois esse nó possui "+pai.getValue()+ "como filhe esquerdo.");
+        }else{
+            aux = new Node(value,pai);
+        }
+
+    }
+    public void addnoderight(int value,Node pai){
+        Node aux;
+        if(pai.hasRightChld() == true){
+            System.out.println("impossivel remover pois esse nó possui "+pai.getValue()+ "como filhe direito.");
+        }else{
+            aux = new Node(value,pai);
+        }
+    }
+
+    public void addNodeFather(Node no){
+        if (upfather == null){
+            upfather = no;
+            System.out.println("Nó inicial adcionado!");
+        }
+    }
+
+
 }
