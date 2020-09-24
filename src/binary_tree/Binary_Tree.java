47572dd9c277712980e61e685c97d41831fc1cd7
caioo <caioo@192.168.0.56>
2020-09-24 00:39:10 -0300

    inserindo métodos vazios e arrumando nomenclaturas.

--- src/binary_tree/Binary_Tree.java
+++ src/binary_tree/Binary_Tree.java
@@ -2,32 +2,48 @@ package binary_tree;
 
 public class Binary_Tree {
 
-    private Node upfather;
+    private Node pai;
 
     public void addNodeLeft(int value,Node pai){
         Node aux;
         if(pai.hasLeftChld() == true){
-            System.out.println("impossivel remover pois esse nó possui "+pai.getValue()+ "como filhe esquerdo.");
+            System.out.println("impossivel adcionar pois esse nó possui "+pai.getValue()+ "como filho esquerdo.");
         }else{
             aux = new Node(value,pai);
+            pai.setLeftnode(aux);
+            aux.setFatherNode(pai);
         }
 
     }
-    public void addnoderight(int value,Node pai){
+    public void addNodeRight(int value,Node pai){
         Node aux;
         if(pai.hasRightChld() == true){
-            System.out.println("impossivel remover pois esse nó possui "+pai.getValue()+ "como filhe direito.");
+            System.out.println("impossivel adcionar pois esse nó possui "+pai.getValue()+ "como filho direito.");
         }else{
             aux = new Node(value,pai);
+            pai.setRightnode(aux);
+            aux.setFatherNode(pai);
         }
     }
 
     public void addNodeFather(Node no){
-        if (upfather == null){
-            upfather = no;
+        if (pai == null){
+            pai = no;
             System.out.println("Nó inicial adcionado!");
         }
     }
 
+    public void profundidadeNo(Node no){
+//numero de ancestrais
+    }
+    public void alturaNo(Node no){
+//numero de descendentes
+    }
+    public void grauNo(Node no){
+
+    }
+    public void qtdNo(){
+
+    }
 
 }
